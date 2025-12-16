5c604199ad9f2afc7334de519d29e5931d018236
Lola-Ojabowale <lola.ojabowale@gmail.com>
2025-12-16 09:58:06 -0500

    Clean up: remove unused demo files, rooms, transcribe modules, and workflows

--- flow/rooms/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2025 Lunch Pail Labs, LLC
-# Licensed under the Apache License, Version 2.0
-
-"""
-Rooms module - PailKit API
-
-Contains room configuration, profiles, and builders.
-"""
--- flow/transcribe/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2025 Lunch Pail Labs, LLC
-# Licensed under the Apache License, Version 2.0
-
-"""
-Transcription module - PailKit API
-
-Contains transcription configuration, profiles, and builders.
-"""
